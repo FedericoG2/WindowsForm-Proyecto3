dd3b8f290f756b46519e0bab72516b87bbdbeec2
FedericoG2 <fg0170700@gmail.com>
2023-10-02 19:16:02 -0300

    Agregar archivos de proyecto.

---
+++ Ciclos/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Ciclos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDesde = new System.Windows.Forms.TextBox();
+            this.txtHasta = new System.Windows.Forms.TextBox();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnPromedio = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.optImpares = new System.Windows.Forms.RadioButton();
+            this.optPares = new System.Windows.Forms.RadioButton();
+            this.optTodo = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desde";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasta";
+            // 
+            // txtDesde
+            // 
+            this.txtDesde.Location = new System.Drawing.Point(109, 31);
+            this.txtDesde.Name = "txtDesde";
+            this.txtDesde.Size = new System.Drawing.Size(100, 20);
+            this.txtDesde.TabIndex = 2;
+            // 
+            // txtHasta
+            // 
+            this.txtHasta.Location = new System.Drawing.Point(109, 72);
+            this.txtHasta.Name = "txtHasta";
+            this.txtHasta.Size = new System.Drawing.Size(100, 20);
+            this.txtHasta.TabIndex = 3;
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Location = new System.Drawing.Point(375, 25);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(83, 31);
+            this.btnSuma.TabIndex = 4;
+            this.btnSuma.Text = "&Suma";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnPromedio
+            // 
+            this.btnPromedio.Location = new System.Drawing.Point(375, 75);
+            this.btnPromedio.Name = "btnPromedio";
+            this.btnPromedio.Size = new System.Drawing.Size(83, 31);
+            this.btnPromedio.TabIndex = 5;
+            this.btnPromedio.Text = "&Promedio";
+            this.btnPromedio.UseVisualStyleBackColor = true;
+            this.btnPromedio.Click += new System.EventHandler(this.btnPromedio_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Resultado";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(109, 117);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.optImpares);
+            this.groupBox1.Controls.Add(this.optPares);
+            this.groupBox1.Controls.Add(this.optTodo);
+            this.groupBox1.Location = new System.Drawing.Point(236, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(109, 106);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Opciones";
+            // 
+            // optImpares
+            // 
+            this.optImpares.AutoSize = true;
+            this.optImpares.Location = new System.Drawing.Point(18, 67);
+            this.optImpares.Name = "optImpares";
+            this.optImpares.Size = new System.Drawing.Size(62, 17);
+            this.optImpares.TabIndex = 2;
+            this.optImpares.TabStop = true;
+            this.optImpares.Text = "Impares";
+            this.optImpares.UseVisualStyleBackColor = true;
+            // 
+            // optPares
+            // 
+            this.optPares.AutoSize = true;
+            this.optPares.Location = new System.Drawing.Point(18, 44);
+            this.optPares.Name = "optPares";
+            this.optPares.Size = new System.Drawing.Size(52, 17);
+            this.optPares.TabIndex = 1;
+            this.optPares.TabStop = true;
+            this.optPares.Text = "Pares";
+            this.optPares.UseVisualStyleBackColor = true;
+            // 
+            // optTodo
+            // 
+            this.optTodo.AutoSize = true;
+            this.optTodo.Location = new System.Drawing.Point(18, 19);
+            this.optTodo.Name = "optTodo";
+            this.optTodo.Size = new System.Drawing.Size(50, 17);
+            this.optTodo.TabIndex = 0;
+            this.optTodo.TabStop = true;
+            this.optTodo.Text = "Todo";
+            this.optTodo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 191);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnPromedio);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.txtHasta);
+            this.Controls.Add(this.txtDesde);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Sumatorias y Promedio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDesde;
+        private System.Windows.Forms.TextBox txtHasta;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnPromedio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton optImpares;
+        private System.Windows.Forms.RadioButton optPares;
+        private System.Windows.Forms.RadioButton optTodo;
+    }
+}
+
---
+++ Ciclos/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ciclos
+{
+    public partial class Form1 : Form
+    {
+        int desde;
+        int hasta;
+        int resultado = 0;
+        float promedio = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtDesde.Text = "";
+            txtHasta.Text = "";
+            txtResultado.Text = "";
+            optTodo.Checked = true;
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+           //Valor desde que pone el usuario
+            desde = int.Parse(txtDesde.Text);
+
+           //Valor hasta que pone el usuario
+            hasta = int.Parse(txtHasta.Text);
+
+            if(desde != 0 && hasta != 0) {
+               
+                //Si esta seleccionado todo
+                if(optTodo.Checked)
+                {
+                    for(int i = desde; i <= hasta; i++)
+                    {
+                        resultado += i;
+                    }
+                    promedio = (float)resultado / hasta;
+
+                }
+                //Si esta seleccionado pares
+                if (optPares.Checked)
+                {
+                    for (int i = desde; i <= hasta; i++)
+                    {
+                        //Antes de sumar se pregunta si el valor de i que va cambiando es par
+                        if(i%2 == 0)
+                        {
+                            resultado += i;
+                        }
+                        
+                    }
+                    promedio = (float)resultado / hasta;
+
+                }
+                //Si esta seleccionado impares
+                if (optImpares.Checked)
+                {
+                    for (int i = desde; i <= hasta; i++)
+                    {
+                        //Antes de sumar se pregunta si el valor de i que va cambiando es impar
+                        if ( i % 2 != 0)
+                        {
+                            resultado += i;
+                        }
+
+                    }
+                    promedio = (float)resultado / hasta;
+
+                }
+            }
+
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void btnPromedio_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(promedio.ToString()); 
+        }
+    }
+}
